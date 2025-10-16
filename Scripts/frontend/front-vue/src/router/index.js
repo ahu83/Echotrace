246c2ca6313637fae1b10ec94a852f73f8919f9b
Guanxiang Zhao <1099124504@qq.com>
2025-10-16 19:16:11 +0800

    add login, index, signup, forgot, forgotsent pages

--- Scripts/frontend/front-vue/src/router/index.js
+++ Scripts/frontend/front-vue/src/router/index.js
@@ -6,10 +6,35 @@ Vue.use(VueRouter)
 
 const TextToSpeech = () => import('@/views/TextToSpeech.vue');
 const DetectionResult = () => import('@/views/DetectionResult.vue');
+const SignUp = () => import('@/views/SignUp.vue');
+const index = () => import('@/views/index.vue');
+const login  = () => import('@/views/login.vue');
+const Forgot = () => import('@/views/Forgot.vue');
+const ForgotSent = () => import('@/views/ForgotSent.vue');
 
 const routes = [
     {
         path: '/',
+        name: 'index',
+        component: index
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: login
+    },
+    {
+        path: '/forgot',
+        name: 'forgot',
+        component: Forgot
+    },
+    {
+        path: '/forgotSent',
+        name: 'forgotSent',
+        component: ForgotSent
+    },
+    {
+        path: '/home',
         name: 'home',
         component: HomeView
     },
@@ -22,6 +47,11 @@ const routes = [
         path: '/result',
         name: 'DetectionResult',
         component: DetectionResult
+    },
+    {
+        path: '/signup',
+        name: 'SignUp',
+        component: SignUp
     },
 ]
 
